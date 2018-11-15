8b5c4ec4ec0ede8e2276b9dabc078659ba807900
hanksterhan <henryhan62@gmail.com>
2018-11-15 15:47:38 -0500

    finished implementing GA, now build a dash app to host this code and create a front end interaction

--- script.py
+++ script.py
@@ -1,5 +1,10 @@
 import random
 import operator
+import argparse
+import matplotlib.pyplot as plt
+import time
+
+tempstart = time.time()
 
 def generateAWord(length):
     i = 0
@@ -35,7 +40,7 @@ def selectFromPopulation(populationSorted, best_sample, lucky_few):
 
 def createChild(individual1, individual2):
     child = ""
-    for i in range(len(individual)):
+    for i in range(len(individual1)):
         if (int(100 *random.random()) < 50):
             child += individual1[i]
         else:
@@ -44,7 +49,7 @@ def createChild(individual1, individual2):
 
 def createChildren(breeders, number_of_children):
     nextPopulation = []
-    for i in range(len(breeders//2)):
+    for i in range(len(breeders)//2):
         for j in range(number_of_children):
             nextPopulation.append(createChild(breeders[i], breeders[len(breeders) - 1 - i]))
     return nextPopulation
@@ -78,7 +83,7 @@ def multipleGeneration(number_of_generation, password, size_population, best_sam
     return historic
 
 def getBestIndividualFromPopulation(population, password):
-    return computerPerfPopulation(population, password)[0]
+    return computePerfPopulation(population, password)[0]
 
 def getListBestIndividualFromHistorique(historic, password):
     bestIndividuals = []
@@ -99,8 +104,59 @@ def fitness(password, test_word):
             i += 1
         return score * 100 / len(password)
 
+def printResult(historic, password, number_of_generation):
+    result = getListBestIndividualFromHistorique(historic, password)[number_of_generation-1]
+    print ("solution: \"" + result[0] + "\", fitness: " + str(result[1]))
+
+#graph
+def evolutionBestFitness(historic, password):
+    plt.axis([0,len(historic),0,105])
+    plt.title(password)
+
+    evolutionFitness = []
+    for population in historic:
+        evolutionFitness.append(getBestIndividualFromPopulation(population, password)[1])
+    plt.plot(evolutionFitness)
+    plt.ylabel('fitness best individual')
+    plt.xlabel('generation')
+    plt.show()
+
+def evolutionAverageFitness(historic, password, size_population):
+    plt.axis([0,len(historic),0,105])
+    plt.title(password)
+
+    evolutionFitness = []
+    for population in historic:
+        populationPerf = computePerfPopulation(population, password)
+        averageFitness = 0
+        for individual in populationPerf:
+        	averageFitness += individual[1]
+        evolutionFitness.append(averageFitness/size_population)
+    plt.plot(evolutionFitness)
+    plt.ylabel('Average fitness')
+    plt.xlabel('generation')
+    plt.show()
+
 def main():
-    
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-p", "--password", required=True, help="The password to be cracked by the algorithm")
+    parser.add_argument("-s", "--size_of_population", type=int, default=100, help="The size of the population")
+    parser.add_argument("-l", "--lucky_few", type= int, default=20, help="The number of passwords to be randomly passed on in each generation")
+    parser.add_argument("-b", "--best_sample", type=int, default=20, help="The number of the top passwords to be passed on in each generation")
+    parser.add_argument("-c", "--num_children", type=int, default=5, help="The number of children in each generation")
+    parser.add_argument("-n", "--num_generation", type=int, default=50, help="The number of generations in the algorithm")
+    parser.add_argument("-m", "--chance_of_mutation", type=int, default=5, help="The percentage chance of mutation [0<m<100]")
+    args = parser.parse_args()
+
+    if ((args.best_sample + args.lucky_few) / 2 * args.num_children != args.size_of_population):
+        print("pouplation size not stable")
+    else:
+        historic = multipleGeneration(args.num_generation, args.password, args.size_of_population, args.best_sample, args.lucky_few, args.num_children, args.chance_of_mutation)
+        printResult(historic, args.password, args.num_generation)
+        evolutionBestFitness(historic, args.password)
+        evolutionAverageFitness(historic, args.password, args.size_of_population)
+    print(time.time() - tempstart)
 
 if __name__ == "__main__":
     main()
