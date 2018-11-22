e2affeb70398dd369e130fb7dc3b50717d59e4cb
hank <henryhan62@gmail.com>
2018-11-22 18:59:42 -0500

    finished stylizing the app and wrote a description, README file is next

--- app.py
+++ app.py
@@ -9,45 +9,77 @@ from PasswordCracker import *
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
+intro1 = 'A Genetic Algorithm is an algorithm inspired by the process of natural selection. They are \
+                commonly used in optimization and search problems. The idea of a genetic algorithm is to \
+                start with a random population, choose the best members in the population by a certain heurestic,\
+                "breeding" those members to generate the next generation of members, and iterating until some \
+                convergence or for some number of generations. In this project, I implemented a genetic algorithm \
+                as a password cracker to try to generate a target string.'
+intro2 = 'Adjust the values on the left and click "RUN" to see how they affect the genetic algorithm\'s \
+                best guess at the password. A \'fitness\' score is a measure of how close a member matches the \
+                target password. In each generation, the members with the highest fitness scores and some random lucky \
+                members are chosen to be parents and "breed" to generate the members in the next generation. \
+                The graph generated on the right displays the best \
+                individual fitness score per generation and the average fitness score per generation. \
+                Hover over data points to see the best/average fitness score of a particular generation.'
+
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div(children=[
-    html.H1(children='Password Cracking Using a Genetic Algorithm'),
+    html.H1('Password Cracking Using a Genetic Algorithm'),
+    html.P(intro1),
+    html.P(intro2),
+    html.Div([
+        html.Div([
+            html.Label('Target Password - password for the genetic algorithm to learn'),
+            dcc.Input(id='password', value='password', type='text'),
 
-    html.Div(children='Adjust the values to see how they affect the genetic algorithm.'),
+            html.Label('Population Size - number of members per generation'),
+            dcc.Input(id='size_of_population', value=100, type='number'),
 
-    html.Label('Target Password'),
-    dcc.Input(id='password', value='password', type='text'),
+            html.Label('Best Sample - the number of most fit members in a generation selected to breed'),
+            dcc.Input(id='best_sample', value= 20, type='number'),
 
-    html.Label('Population Size'),
-    dcc.Input(id='size_of_population', value=100, type='number'),
+            html.Label('Lucky Few - the number of random members in a generation selected to breed'),
+            dcc.Input(id='lucky_few', value=20, type='number'),
 
-    html.Label('Lucky Few'),
-    dcc.Input(id='lucky_few', value=20, type='number'),
+            html.Label('Number of Children - number of children each parent can breed'),
+            dcc.Input(id='num_children', value= 20, type='number'),
 
-    html.Label('Best Sample'),
-    dcc.Input(id='best_sample', value= 20, type='number'),
+            html.Label('Number of Generations - number of generations in the genetic algorithm'),
+            dcc.Input(id='num_generation', value= 50, type='number'),
 
-    html.Label('Number of Children'),
-    dcc.Input(id='num_children', value= 20, type='number'),
+            html.Label('Chance of Mutation - percent chance that there is a mutation when breeding'),
+            dcc.Input(id='chance_of_mutation', value= 5, type='number'),
 
-    html.Label('Number of Generations'),
-    dcc.Input(id='num_generation', value= 50, type='number'),
+            html.Button(id="submit-button", children="Run"),
 
-    html.Label('Chance of Mutation'),
-    dcc.Input(id='chance_of_mutation', value= 5, type='number'),
+            html.Div(id='my-div'),
+        ], style={
+            'backgroundColor': 'rgb(255,255,255)',
+            'padding': '10px 10px',
+            'width':'49%',
+            'float':'left'
+        }),
+        html.Div([    
+            dcc.Graph(id='genetics-graph'),
 
-    html.Button(id="submit-button", children="Run"),
-
-    html.Div(id='my-div'),
-    html.Div(id='my-graph'),
-
-    dcc.Graph(id='genetics-graph'),
-
-    # Hidden div inside the app that stores the intermediate json of the object
-    html.Div(id='intermediate-value', style={'display':'none'})
-    
-])
+            # Hidden div inside the app that stores the intermediate json of the object
+            html.Div(id='intermediate-value', style={'display':'none'})
+        ], style={
+            'margin-left': '55%',
+        })
+    ],style={
+        'width':'90%',
+        'margin-left': '5%',
+        'margin-top': '5%',
+        'display':'inline-block',
+    })
+], style={
+    'backgroundColor': 'rgb(220,220,220)',
+    'margin-top': '5%',
+    'padding': '10px 10px',
+})
 
 # callback to run algorithm and store the object in the hidden Div
 # activated when the "Run" button is clicked
@@ -137,6 +169,7 @@ def update_graph(n_clicks, obj_json, password):
             name='Best Individual Fitness'
             )],
         'layout': go.Layout(
+            title='Best and Average Fitness Score by Generation',
             xaxis={
                 'title': 'Generation',
                 'type': 'linear'
@@ -145,8 +178,8 @@ def update_graph(n_clicks, obj_json, password):
                 'title': 'Fitness Score',
                 'type': 'linear'
             },
-            margin={'l': 40, 'b': 40, 't': 10, 'r': 0},
-            hovermode='closest'
+            hovermode='closest',
+            legend=dict(x=1, y=-0.2, orientation="h")
         )
     }
 
